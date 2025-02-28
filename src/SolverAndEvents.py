205cf0684fa04c553ea69b4507940ca587e4d6c4
Richard von Eckardstein <richard.voneckardstein@uni-muenster.de>
2025-02-28 12:21:54 +0100

    Adding todo to SolverAndEvents

--- src/SolverAndEvents.py
+++ src/SolverAndEvents.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.integrate import solve_ivp
-
+#ToDo: Messages names after events.
 
 #Errors
 class EventError(Exception):
@@ -14,8 +14,10 @@ class Event:
     ### finish: finalise the solver
     ### repeat: repeat the last iteration of the solver
     ### proceed: continue solving from the point of termination
-    #An event may pass the secondary order "update" to the solver. This can update the following attributes of the solver:
+    #Any event may pass the secondary order "update" to the solver. This can update the following attributes of the solver:
     ### __ODE__, __tend, __atol, __rtol, __method 
+    
+    #Terminal event consequences should distinguish the cases: termination on this event, termination not on this event, termination without event.
     def __init__(x, name, func, terminal, direction, consequence):
         x.name = name
         x.func = func
@@ -26,8 +28,7 @@ class Event:
 
         x.occurance = 0
         x.consequence = consequence
-        return 
-
+        return
     
     def EventConsequence(x, sol, occurance):
         result = x.consequence(sol, occurance)
