704287c5442b8f3a7e59ad29f592d1f0088596b2
Richard von Eckardstein <richard.voneckardstein@uni-muenster.de>
2025-08-07 16:18:54 +0200

    Removing Default Variables

--- src/BGQuantities/DefaultVariables.py
+++
@@ -1,8 +0,0 @@
-from src.BGQuantities.BGTypes import BGVal
-
-#Space--time variables:
-t=BGVal("t", -1, 0) #physical time
-N=BGVal("N", 0, 0) #e-folds
-a=BGVal("a", 0, 0) #scale factor
-H=BGVal("H", 1, 0) #Hubble rate
-spacetime = {t, N, a, H}
